5d8ced0dc07a14f0370783dd711c1e35e7b9b49c
mahendra8991 <govindumahendra@7gmail.com>
2023-05-18 06:39:34 +0000

    code-assignment-6

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import RpsPage from './components/RpsPage'
+
+import './App.css'
+
+const choicesList = [
+  {
+    id: 'ROCK',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/rock-image.png',
+  },
+  {
+    id: 'SCISSORS',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/scissor-image.png',
+  },
+  {
+    id: 'PAPER',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/paper-image.png',
+  },
+]
+
+const App = () => <RpsPage />
+/*<div>Hello World</div>*/
+
+export default App
